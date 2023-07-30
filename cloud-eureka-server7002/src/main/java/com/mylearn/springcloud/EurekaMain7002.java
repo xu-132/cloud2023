c1de9b4292636dc12ec79f4c0ad65b89aac14064
xuwenjing <xuwenjing@viewhigh.com>
2023-07-30 11:08:03 +0800

    Eureka集群环境构建，将支付服务8001微服务和将订单服务80微服务发布到2台Eureka集群配置中

--- cloud-eureka-server7002/src/main/java/com/mylearn/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/mylearn/springcloud/EurekaMain7002.java
@@ -1,17 +1,18 @@
 package com.mylearn.springcloud;
 
+import org.slf4j.helpers.MarkerIgnoringBase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author xu
- * @date 2023/7/29
+ * @date 2023/7/30
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/mylearn/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/mylearn/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.mylearn.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xu
  * @date 2023/7/28
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
