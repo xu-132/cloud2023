6c4b8acaa0b5073bbf1fa7702b36b7846b0c202f
xuwenjing <xuwenjing@viewhigh.com>
2023-08-05 16:39:28 +0800

    SpringCloud整合Zookeeper替代Eureka

---
+++ cloud-consumerzk-order80/src/main/java/com/mylearn/springcloud/OrderZKMain80.java
@@ -0,0 +1,17 @@
+package com.mylearn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xu
+ * @date 2023/8/5
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class,args);
+    }
+}
