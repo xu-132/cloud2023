86fea6eafd0c31cb94855e6bf29a592957aa6266
xuwenjing <xuwenjing@viewhigh.com>
2023-08-07 23:24:34 +0800

    OpenFeign服务接口调用(OpenFeign使用步骤)

---
+++ cloud-consumer-feign-order80/src/main/java/com/mylearn/springcloud/OrderFeignMain80.java
@@ -0,0 +1,17 @@
+package com.mylearn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author xu
+ * @date 2023/8/7
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/mylearn/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,19 @@
+package com.mylearn.springcloud.service;
+
+import com.mylearn.springcloud.entities.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author xu
+ * @date 2023/8/7
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id);
+}
