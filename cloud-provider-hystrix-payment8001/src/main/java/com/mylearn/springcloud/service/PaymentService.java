930eb301f307f98f816a71e2abc5db8dc95bf641
xuwenjing <xuwenjing@viewhigh.com>
2023-08-09 15:42:38 +0800

    Hystrix熔断器(hystrix案例-服务熔断)

--- cloud-provider-hystrix-payment8001/src/main/java/com/mylearn/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/mylearn/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.mylearn.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -42,7 +44,31 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + "  id: " + id +"\t" + "O(∩_∩)O哈哈~" + "耗时(秒): ";
     }
 
+    //处理方法（服务降级的兜底解决方案）
     public String paymentInfo_TimeOutHandler(Integer id){
         return "线程池：" + Thread.currentThread().getName() + "  8001系统繁忙或者运行报错，请稍后再试，id: " + id +"\t" + "o(╥﹏╥)o";
     }
+
+    //======服务熔断，上边是服务降级
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),// 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),// 时间窗口期(时间范围)
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")// 失败率达到多少后跳闸
+
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if(id < 0){
+            throw new RuntimeException("******id 不能负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号：" + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "id 不能负数，请稍后再试，o(╥﹏╥)o  id: " + id;
+    }
+
+
+
 }
