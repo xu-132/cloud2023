13003eb08c49c35c68c8dac697e68fd67bde3de7
xuwenjing <xuwenjing@viewhigh.com>
2023-08-09 16:08:02 +0800

    Hystrix熔断器(服务监控hystrixDashboard)

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/mylearn/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.mylearn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author xu
+ * @date 2023/8/9
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class);
+    }
+}
