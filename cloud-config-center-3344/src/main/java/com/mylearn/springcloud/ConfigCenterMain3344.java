d292c9a51503bf6dc28181906e3082aa13a3b4ad
xuwenjing <xuwenjing@viewhigh.com>
2023-08-11 16:34:42 +0800

    SpringCloud config分布式配置中心(Config服务端配置与测试，实现了SpringCloudConfig通过Github获取配置信息)

---
+++ cloud-config-center-3344/src/main/java/com/mylearn/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.mylearn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Cloud的配置中心模块 主启动类
+ * @author xu
+ * @date 2023/8/11
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
