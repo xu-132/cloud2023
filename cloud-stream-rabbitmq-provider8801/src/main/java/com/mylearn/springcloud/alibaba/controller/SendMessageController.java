48ebc41e19620b2bda22a536ed4a8a31419160c1
xuwenjing <xuwenjing@viewhigh.com>
2023-08-14 11:50:00 +0800

    SpringCloud Alibaba Nacos服务注册和配置中心(Nacos作为服务注册中心演示)

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/mylearn/springcloud/alibaba/controller/SendMessageController.java
@@ -0,0 +1,22 @@
+package com.mylearn.springcloud.alibaba.controller;
+
+import com.mylearn.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author xu
+ * @date 2023/8/12
+ */
+@RestController
+public class SendMessageController {
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+}
---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/mylearn/springcloud/alibaba/OrderNacosMain83.java
@@ -0,0 +1,17 @@
+package com.mylearn.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xu
+ * @date 2023/8/14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class,args);
+    }
+}
